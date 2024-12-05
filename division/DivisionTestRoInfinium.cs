622518e2930d75b199b633012d95b2f62975da79
AndryMaster <rusandrkozak@gmail.com>
2024-12-05 09:54:25 +0300

    Final commit (working fast + savings)

--- division/DivisionTestRoInfinium.cs
+++ division/DivisionTestRoInfinium.cs
@@ -1,42 +1,116 @@
 ﻿using System.Diagnostics;
 using System.Numerics;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace division;
 
 internal sealed class DivisionTestRoInfinium : Division
 {
+    public static bool useFileSaving = false;
+    
+    private class SaveState
+    {
+        [JsonPropertyName("isCorrect")]
+        public bool isCorrect { get; set; }
+        
+        [JsonPropertyName("lastRo")]
+        public int lastRo { get; set; }
+        
+        [JsonPropertyName("Modules")]
+        public uint[] mods { get; set; }
+        
+        [JsonPropertyName("P")]
+        public long P { get; set; }
+        
+        [JsonPropertyName("allTimeMs")]
+        public long allTime { get; set; }
+    }
+
+    private static void SaveHistory(Dictionary<long, SaveState> saveStates)
+    {
+        if (useFileSaving)
+        {
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            
+            using (FileStream fs = new FileStream("save.json", FileMode.Create, FileAccess.Write))
+            {
+                JsonSerializer.Serialize(fs, saveStates, options);
+            }
+        }
+    }
+    
+    private static Dictionary<long, SaveState> LoadHistory()
+    {
+        if (useFileSaving && File.Exists(@"save.json"))
+        {
+            using (FileStream fs = new FileStream("save.json", FileMode.Open, FileAccess.Read))
+            {
+                Dictionary<long, SaveState>? saveStates = JsonSerializer.Deserialize<Dictionary<long, SaveState>>(fs);
+                return saveStates ?? new();
+            }
+        }
+        return new();
+    }
+
+    private static void ClearHistory()
+    {
+        if (File.Exists(@"save.json"))
+            File.Delete(@"save.json");
+    }
+    
     public static void Test(uint[] newModules)
     {
         initModules(newModules);
-
-        Console.Write($"\n\nP={P} ro=??? mods=[ ");
+        
+        long needIters = 0;
+        for (long i = 1; i <= (P + 1) / 2; i++) needIters += P / i - 1;
+        Console.Write($"\n\nP={P} ({needIters}) ro=<???> mods=[ ");
         foreach (int i in Modules)
             Console.Write($"{i}, ");
-        Console.WriteLine("]\n");
+        Console.WriteLine($"]\nNeed iters={needIters}\n");
         Console.WriteLine(DateTime.Now.ToLongTimeString());
 
         Stopwatch sw = new Stopwatch();
         Stopwatch swFull = new Stopwatch();
         swFull.Start();
-
-        int ro = 0;
-        BigInteger[] k;
-        bool useMemory = (P < 1L << 20);
-        bool isCorrect = false;
         
+        var history = LoadHistory();
+        if (!history.ContainsKey(P))
+            history.Add(P, new SaveState { isCorrect = false, lastRo = 0, mods = Modules, P = P, allTime = 0 });
+        
+        int ro = history[P].lastRo;  // 0
+        if (ro != 0) Console.WriteLine($"Restoring last Ro value (={ro})");
+        
+        bool useMemory = (P < 1L << 20); useMemory = false;
+        if (useMemory) Console.WriteLine("Using memory");
+        else Console.WriteLine("Not using memory");
+
+        bool isCorrect = history[P].isCorrect;  // false
         while (!isCorrect)
         {
-            k = calk_k(++ro);
+            BigInteger[] k = calk_k(++ro);
             sw.Restart();
             
             if (useMemory)
                 isCorrect = TestMem(ro, k);
+            else if (ro < 128)
+                isCorrect = TestNoMem128(ro, k);  // TestNoMem(ro, k);
             else
                 isCorrect = TestNoMem(ro, k);
             
-            Console.WriteLine($"Ro={ro}\tTime={sw.ElapsedMilliseconds} ms \tisCorrect={isCorrect}");
+            history[P].lastRo = ro;
+            history[P].isCorrect = isCorrect;
+            history[P].allTime += sw.ElapsedMilliseconds;
+            SaveHistory(history);
+            
+            Console.WriteLine($"Ro={ro}\tTime={sw.ElapsedMilliseconds} " +
+                              $"({history[P].allTime}) ms \tisCorrect={isCorrect}");
         }
-        Console.WriteLine($"Full time: {swFull.ElapsedMilliseconds} ms");
+        Console.WriteLine($"Result: Ro={ro}\nFull time: {swFull.ElapsedMilliseconds} ms");
         Console.WriteLine(DateTime.Now.ToLongTimeString());
     }
 
@@ -97,23 +171,33 @@ internal sealed class DivisionTestRoInfinium : Division
     {
         BigInteger GetF(long i)
         {
-            if (i != P)
-                return F(i, ro, k);
-            return new BigInteger(1) << 127;
+            if (i == P) return BigInteger.One << 255;
+            
+            BigInteger s = 0;
+            
+            for (int j = 0; j < Modules.Length; j++)
+                s += k[j] * (i % Modules[j]);    
+
+            return s - ((s >> ro) << ro);
         }
 
         bool isCorrect = true;
-        
-        CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+
+        using CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
         CancellationToken token = cancelTokenSource.Token;
         
         try
         {
-            Parallel.For(0, P, new ParallelOptions { CancellationToken = token }, i =>
+            Parallel.For(1, (P + 1) / 2 + 1, new ParallelOptions { CancellationToken = token }, i =>
             {
-                if (!(GetF(i) < GetF(i+1)))
+                BigInteger value, valueInit = GetF(i);
+                value = valueInit;
+        
+                for (long j = i * 2; j <= P; j += i)
                 {
-                    cancelTokenSource.Cancel();
+                    value += valueInit;
+                    if (!(GetF(j - 1) < value && value <= GetF(j))) cancelTokenSource.Cancel(); // break;
+                    if (token.IsCancellationRequested) break;
                 }
             });
         }
@@ -121,33 +205,52 @@ internal sealed class DivisionTestRoInfinium : Division
         {
             isCorrect = false;
         }
+        
+        return isCorrect;
+    }
+    
+    private static bool TestNoMem128(int ro, BigInteger[] k)
+    {
+        bool isCorrect = true;
 
-        if (isCorrect)
+        UInt128[] k128 = k.Select(el => (UInt128)el).ToArray();
+        UInt128 GetF(long i)
         {
-            try
-            {
-                Parallel.For(1, (P + 1) / 2 + 1, new ParallelOptions { CancellationToken = token }, i =>
-                {
-                    BigInteger value, valueInit;
-                    value = valueInit = GetF(i);
+            if (i == P) return UInt128.MaxValue;
             
-                    for (long j = i * 2; j <= P; j += i)
-                    {
-                        value += valueInit;
-                        if (!(GetF(j - 1) < value && value <= GetF(j)))
-                        {
-                            cancelTokenSource.Cancel();
-                        }
-                    }
-                });
-            }
-            catch (OperationCanceledException)
-            {
-                isCorrect = false;
-            }
+            UInt128 s = 0;
+            
+            for (int j = 0; j < Modules.Length; j++)
+                s += k128[j] * new UInt128(0, (ulong)i % Modules[j]);;    
+
+            return s - ((s >> ro) << ro);
         }
         
-        cancelTokenSource.Dispose();
+        using CancellationTokenSource cancelTokenSource = new CancellationTokenSource();
+        CancellationToken token = cancelTokenSource.Token;
+        
+        try
+        {
+            Parallel.For(1, (P + 1) / 2 + 1, new ParallelOptions { CancellationToken = token }, i =>
+            {
+                UInt128 value, valueInit = GetF(i);
+                value = valueInit;
+        
+                for (long j = i * 2; j <= P; j += i)
+                {
+                    // Console.WriteLine($"({i}; {j}): \t[{GetF(j - 1)} < {value} <= {GetF(j)}]\t" +
+                    //                   $"({GetF(j - 1) < value && value <= GetF(j)}) ({j/i-1}, {P/i-1})");
+                    value += valueInit;
+                    if (!(GetF(j - 1) < value && value <= GetF(j))) cancelTokenSource.Cancel(); // break;
+                    if (token.IsCancellationRequested) break;
+                }
+            });
+        }
+        catch (OperationCanceledException)
+        {
+            isCorrect = false;
+        }
+        
         return isCorrect;
     }
     
@@ -218,4 +321,20 @@ internal sealed class DivisionTestRoInfinium : Division
     //
     //     if (!isCorrect) break;
     // }
+    
+    
+    // NO MEM 128
+    // try
+    // {
+    //     Parallel.For(0, P, new ParallelOptions { CancellationToken = token }, i =>
+    //     {
+    //         if (!(GetF(i) < GetF(i+1)))
+    //             cancelTokenSource.Cancel();
+    //     });
+    // }
+    // catch (OperationCanceledException)
+    // {
+    //     isCorrect = false;
+    // }
+    // Console.Write("*");
 }
--- division/Program.cs
+++ division/Program.cs
@@ -2,29 +2,65 @@
 
 class Program
 {
-    public const int MaxCountModules = 8;
-    public const int CurrentCountModules = 4;
+    public const int MaxCountModules = 8;      // Old
+    public const int CurrentCountModules = 4;  // Old
     
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         
-        // DivisionTestRoInfinium.Test(new uint[]{2, 3, 5, 7});
-        // DivisionTestRoInfinium.Test(new uint[]{5, 7, 13, 31});
-        // DivisionTestRoInfinium.Test(new uint[]{27, 37, 128});
-        // DivisionTestRoInfinium.Test(new uint[]{125, 127, 128});
+        DivisionTestRoInfinium.useFileSaving = true;
+        CalcAllRo();  // Auto tests
         
-        DivisionTestRoInfinium.Test(new uint[]{125, 127, 128});
-        DivisionTestRoInfinium.Test(new uint[]{27, 29, 31, 32});
-        DivisionTestRoInfinium.Test(new uint[]{123, 125, 127, 128});
-        DivisionTestRoInfinium.Test(new uint[]{507, 509, 511, 512});
-        DivisionTestRoInfinium.Test(new uint[]{121, 123, 125, 127, 128});
-        DivisionTestRoInfinium.Test(new uint[]{2043, 2045, 2047, 2048});
+        // DivisionTestRoInfinium.Test(new uint[]{2, 3});
+        // DivisionTestRoInfinium.Test(new uint[]{2, 3, 5, 7});
+        // DivisionTestRoInfinium.Test(new uint[]{27, 29, 31, 32});
+        // DivisionTestRoInfinium.Test(new uint[]{125, 127, 128});
+        // DivisionTestRoInfinium.Test(new uint[]{125, 127, 128, 3});
+        // DivisionTestRoInfinium.Test(new uint[]{507, 509, 511, 512});
         
         // DivisionTestRo.Test(new uint[]{2, 3, 5, 7});
         // DivisionTestRo.Test(new uint[]{5, 7, 13, 31}, useFast:true);
         // DivisionTestRo.Test(new uint[]{125, 127, 128}, useFast:true);
-        
         // DivisionTestRoOld.Test(new uint[]{125, 127, 128});
+    }
+
+    static void CalcAllRo()
+    {
+        List<uint[]> listMods = new List<uint[]>()
+        {
+            new uint[]{27, 29, 31, 32},
+            // new uint[]{123, 125, 127, 128},
+            new uint[]{507, 509, 511, 512},
+            new uint[]{2043, 2045, 2047, 2048},
+            new uint[]{8187, 8189, 8191, 8192},
+            
+            new uint[]{125, 127, 128},
+            new uint[]{123, 125, 127, 128},
+            new uint[]{121, 123, 125, 127, 128},
+            new uint[]{119, 121, 123, 125, 127, 128},
+            new uint[]{113, 119, 121, 123, 125, 127, 128},
+            new uint[]{109, 113, 119, 121, 123, 125, 127, 128},
+        };
+
+        Dictionary<long, uint[]> tests = new();
+        foreach (uint[] mods in listMods)
+        {
+            long P = 1;
+            foreach (var i in mods) P *= i;
+            tests.Add(P, mods);
+        }
+        
+        Console.WriteLine("\nAll tests:");
+        foreach (var key in tests.Keys.Order())
+        {
+            Console.Write($"{key}: [");
+            foreach (int i in tests[key]) Console.Write($"{i}, ");
+            Console.WriteLine("]");
+        }
+        foreach (var key in tests.Keys.Order())
+        {
+            DivisionTestRoInfinium.Test(tests[key]);
+        }
     }
 }
